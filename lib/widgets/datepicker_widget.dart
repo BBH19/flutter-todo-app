668890494f873f17045b6a07abb5b78ef2e79080
BBH19 <bouchra.hanin1912@gmail.com>
2023-04-06 00:09:35 +0300

    change date picker to date range picker

--- lib/widgets/datepicker_widget.dart
+++ lib/widgets/datepicker_widget.dart
@@ -4,8 +4,9 @@ import 'package:chequeproject/utils/widgect_helper.dart';
 import 'package:chequeproject/widgets/config.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
-class DatePickerWidget extends StatelessWidget {
+class DatePickerWidget extends StatefulWidget {
   Color? focusColor;
   String labeltext;
   String valuetext;
@@ -30,32 +31,63 @@ class DatePickerWidget extends StatelessWidget {
       this.on_changed_function,
       this.enabled})
       : super(key: key);
-  final Object obj; 
+  final Object obj;
+
+  @override
+  State<DatePickerWidget> createState() => _DatePickerWidgetState();
+}
+
+class _DatePickerWidgetState extends State<DatePickerWidget> {
+  var _popupMenuItemIndex = 0;
+
+  var dateRange = DateTimeRange(
+    start: DateTime.now(),
+    //brikolage bricoma
+    end: DateTime.now(),
+  );
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 35,
       child: TextFormField(
-        controller: controller,
-        onTap: () async {
-          DateTime date = DateTime(1900);
-          FocusScope.of(context).requestFocus(FocusNode());
-          date = (await showDatePicker(
-              context: context,
-              initialDate: DateTime.now(),
-              firstDate: DateTime(1900),
-              lastDate: DateTime(2100)))!;
+        controller: widget.controller,
 
-          controller.text = date.toString(); 
+        onTap: () async {
+          DateTimeRange? picked = await showDateRangePicker(
+            context: context,
+            initialDateRange: dateRange,
+            firstDate: DateTime(1900),
+            lastDate: DateTime.now(),
+            builder: (context, child) => Theme(
+              data: ThemeData().copyWith(
+                // ignore: prefer_const_constructors
+                colorScheme: ColorScheme.dark(
+                  primary: Colors.blueGrey,
+                  surface: Colors.white,
+                  onSurface: GlobalParams.GlobalColor,
+                ),
+                dialogBackgroundColor: Colors.black,
+              ),
+              child: child!,
+            ),
+          );
+          // Text(
+          //     "${picked!.start.day}/${picked.start.month}/${picked.start.year}");
+          widget.controller.text = picked!.start.toString();
+          //"${picked!.start.day} - ${picked.start.month} - ${picked.start.year}";
+
+          if (picked != null) {
+            setState(() => {_popupMenuItemIndex = -1, dateRange = picked});
+          }
         },
-        enabled: enabled ?? true,
+        enabled: widget.enabled ?? true,
         cursorColor: GlobalParams.GlobalColor,
-        keyboardType: keyboardType,
-        validator: validator,
-        readOnly: readonly ?? false,
+        keyboardType: widget.keyboardType,
+        validator: widget.validator,
+        readOnly: widget.readonly ?? false,
         decoration: WidgetHelper.getDatePickerDecoration(
-            labeltext, on_changed_function),
+            widget.labeltext, widget.on_changed_function),
         style: const TextStyle(
             fontWeight: FontWeight.w300, fontSize: 14, fontFamily: 'Open Sans'),
         //controller: controller,
