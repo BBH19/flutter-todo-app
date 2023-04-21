49ee1d856c6cde695cf9817e12fc01933d91913a
BBH19 <bouchra.hanin1912@gmail.com>
2023-04-21 16:05:45 +0300

    ajouter swipe_card

--- lib/widgets/itemcard_widget.dart
+++ lib/widgets/itemcard_widget.dart
@@ -1,6 +1,11 @@
 // ignore_for_file: must_be_immutable
+import 'package:chequeproject/blocs/Cheque/cheque_bloc.dart';
+import 'package:chequeproject/views/cheque_edit.dart';
+import 'package:chequeproject/views/cheque_list.dart';
 import 'package:chequeproject/widgets/config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_swipe_action_cell/flutter_swipe_action_cell.dart';
 
 class ItemCard extends StatelessWidget {
   final Size size;
@@ -11,12 +16,13 @@ class ItemCard extends StatelessWidget {
   String? var3;
   String? var4;
   String? var5;
+  SwipeActionCell? swipeActionCell;
   Widget? indicator;
   Widget? additionalinfowidget;
   Color? textcolor;
 
   ItemCard({
-    Key? key,
+    Key? key1,
     required this.size,
     this.color = GlobalParams.MainColor,
     this.var1,
@@ -25,99 +31,125 @@ class ItemCard extends StatelessWidget {
     this.var4,
     this.var5,
     this.onPressed,
+    this.swipeActionCell,
     this.textcolor = GlobalParams.itemCardTextColor,
     this.indicator,
-  }) : super(key: key);
+  }) : super(key: key1);
 
   @override
   Widget build(BuildContext context) {
+    BuildContext _context = context;
+    var currentItem = Cheques;
     return Tooltip(
-      message: var1 ?? "",
-      child: InkWell(
-        onDoubleTap: onPressed,
-        child: SizedBox(
-          width: double.infinity,
-          height: GlobalParams.getItemCardHeight(size),
-          child: Card(
-            elevation: 4,
-            shadowColor: Colors.grey[300],
-            color: color,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        child: Text(
-                          var1 ?? "",
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              color: textcolor,
-                              fontSize: GlobalParams.itemCardFontSize,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: GlobalParams.MainfontFamily),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Text(
-                        var2 ?? "",
-                        overflow: TextOverflow.ellipsis,
-                        textAlign: TextAlign.end,
-                        style: TextStyle(
-                            color: textcolor,
-                            fontSize: GlobalParams.itemCardFontSize,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: GlobalParams.MainfontFamily),
-                      ),
-                    ],
+        message: var1 ?? "",
+        child: InkWell(
+          onDoubleTap: onPressed,
+          child: SizedBox(
+            width: double.infinity,
+            height: GlobalParams.getItemCardHeight(size),
+            child: Card(
+              elevation: 4,
+              shadowColor: Colors.grey[300],
+              color: color,
+              child: SwipeActionCell(
+                key: ObjectKey(key),
+                backgroundColor: color,
+                trailingActions: <SwipeAction>[
+                  SwipeAction(
+                    //title: 'Modifier',
+                    color: Colors.white,
+                    icon: Icon(
+                      Icons.edit,
+                      color: GlobalParams.GlobalColor,
+                    ),
+                    onTap: (handler) {
+                      // Navigator.push(_context,
+                      //     MaterialPageRoute(builder: (context) {
+                      //   return BlocProvider.value(
+                      //       value: BlocProvider.of<ChequeBloc>(_context),
+                      //       child: ChequeEditPage(
+                      //         currentObj: currentItem,
+                      //       ));
+                      // }));
+                      handler(true);
+                    },
                   ),
-                  Row(
+                ],
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        var3 ?? "",
-                        style: TextStyle(
-                            color: textcolor,
-                            fontSize: GlobalParams.itemCardFontSize,
-                            fontWeight: FontWeight.w300,
-                            fontFamily: GlobalParams.MainfontFamily),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            child: Text(
+                              var1 ?? "",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  color: textcolor,
+                                  fontSize: GlobalParams.itemCardFontSize,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: GlobalParams.MainfontFamily),
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Text(
+                            var2 ?? "",
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.end,
+                            style: TextStyle(
+                                color: textcolor,
+                                fontSize: GlobalParams.itemCardFontSize,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: GlobalParams.MainfontFamily),
+                          ),
+                        ],
                       ),
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          indicator ?? const Text(""),
                           Text(
-                            var4 ?? "",
-                            // 'Pr : ${inventoryDetails.singlePrice.toStringAsFixed(2)}',
+                            var3 ?? "",
                             style: TextStyle(
                                 color: textcolor,
                                 fontSize: GlobalParams.itemCardFontSize,
                                 fontWeight: FontWeight.w300,
                                 fontFamily: GlobalParams.MainfontFamily),
                           ),
-                          Text(
-                            var5 ?? "",
-                            //'Qty:${inventoryDetails.qty.toStringAsFixed(3)}',
-                            style: TextStyle(
-                                color: textcolor,
-                                fontSize: GlobalParams.itemCardFontSize,
-                                fontWeight: FontWeight.w300,
-                                fontFamily: GlobalParams.MainfontFamily),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              indicator ?? const Text(""),
+                              Text(
+                                var4 ?? "",
+                                style: TextStyle(
+                                    color: textcolor,
+                                    fontSize: GlobalParams.itemCardFontSize,
+                                    fontWeight: FontWeight.w300,
+                                    fontFamily: GlobalParams.MainfontFamily),
+                              ),
+                              Text(
+                                var5 ?? "",
+                                style: TextStyle(
+                                    color: textcolor,
+                                    fontSize: GlobalParams.itemCardFontSize,
+                                    fontWeight: FontWeight.w300,
+                                    fontFamily: GlobalParams.MainfontFamily),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
+    //);
   }
 }
